ee034aba3e1f0b3909e35da8273eff65e28f1787
zywczak <123404938+zywczak@users.noreply.github.com>
2026-01-07 20:08:30 +0100

    test config

--- vite.config.ts
+++ vite.config.ts
@@ -27,8 +27,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public', 
   define: {
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+  },
+  server: {
+    cors: true
+  },
+  preview: {
+    cors: true,
+    port: 80,
+    host: '0.0.0.0'
   },
   build: {
     lib: {
