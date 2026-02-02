4a1ce14db4a7e5d1a599e6d553dbb483404babf0
Piotr Żywczak <123404938+zywczak@users.noreply.github.com>
2026-02-02 16:59:54 +0100

    Update adress.ts

--- src/api/adress.ts
+++ src/api/adress.ts
@@ -1,3 +1,3 @@
-// const adress = "https://precious-choux-6bd82f.netlify.app"
-const adress = "http://localhost:5173"
+const adress = "https://peppy-tulumba-76cfd0.netlify.app"
+// const adress = "http://localhost:5173"
 export default adress
