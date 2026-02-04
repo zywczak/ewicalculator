c26d3eca815a4c6154683bb32dd3a0b68b35a711
Piotr Żywczak <123404938+zywczak@users.noreply.github.com>
2026-02-04 12:06:13 +0100

    Update address to production URL

--- src/api/adress.ts
+++ src/api/adress.ts
@@ -1,3 +1,4 @@
 // const adress = "https://peppy-tulumba-76cfd0.netlify.app"
-const adress = "http://localhost:5173"
+// const adress = "http://localhost:5173"
+const adress = "https://ewicalculator.vercel.app"
 export default adress
